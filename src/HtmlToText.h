dde6b496cc65a22dcab00b8beb01baa0a05c2426
M <agence.locale@gmail.com>
2026-09-12 22:52:51 +0200

    fix info dialog (rapid xml does not support utf-16)

--- src/HtmlToText.h
+++ src/HtmlToText.h
@@ -1,82 +1,118 @@
 #pragma once
-
-#include <RapidXml/rapidxml.hpp>
-
 #include <string>
+#include <string_view>
+#include <unordered_map>
 
-inline void html_to_text(rapidxml::xml_node<wchar_t>* node, std::wstring& out, int listLevel = 0)
+inline std::wstring decode_escape(std::wstring_view esc)
 {
-  using namespace rapidxml;
-  if (!node) return;
+    static const std::unordered_map<std::wstring_view, std::wstring_view> map = {
+        {L"nbsp",  L" "},
+        {L"amp",   L"&"},
+        {L"lt",    L"<"},
+        {L"gt",    L">"},
+        {L"quot",  L"\""},
+        {L"apos",  L"'"},
+    };
 
-  switch (node->type())
-  {
-    case node_type::node_data:
-      out += node->value();
-      return;
+    if (auto it = map.find(esc); it != map.end())
+        return std::wstring(it->second);
 
-    case node_type::node_element:
-    {
-      std::wstring name = node->name();
-
-      if (name == L"p")
-      {
-          out += L"\n\n";  // paragraph break
-      }
-      else if (name == L"br")
-      {
-          out += L"\n";
-      }
-      else if (name == L"li")
-      {
-          out += std::wstring(listLevel * 2, ' ') + L"- ";
-      }
-      else if (name == L"ul" || name == L"ol")
-      {
-          listLevel++;
-      }
-      else if (name == L"h1" || name == L"h2" || name == L"h3")
-      {
-          out += L"\n";
-      }
-      else if (name == L"script" || name == L"style")
-      {
-          return; // ignore
-      }
-
-      // Recurse into children
-      for (xml_node<wchar_t>* child = node->first_node(); child; child = child->next_sibling())
-      {
-          html_to_text(child, out, listLevel);
-      }
-
-      // After closing certain tags
-      if (name == L"p" || name == L"h1" || name == L"h2" || name == L"h3")
-      {
-          out += L"\n";
-      }
-
-      return;
+    // numeric escapes: &#123; or &#x1F;
+    if (!esc.empty() && esc[0] == L'#') {
+        bool hex = (esc.size() > 1 && (esc[1] == L'x' || esc[1] == L'X'));
+        try {
+            unsigned long code =
+                hex ? std::stoul(std::wstring(esc.substr(2)), nullptr, 16)
+                    : std::stoul(std::wstring(esc.substr(1)), nullptr, 10);
+            return std::wstring(1, static_cast<wchar_t>(code));
+        } catch (...) {}
     }
-    default:
-      return;
-  }
+
+    // unknown escape → return literally "&xxx;"
+    return L"&" + std::wstring(esc) + L";";
 }
 
-inline std::wstring html_to_text(std::wstring const& data)
+inline std::wstring html_to_text(std::wstring_view html)
 {
-  using namespace rapidxml;
-  xml_document<wchar_t> doc;    // character type defaults to char
-  wchar_t* text = const_cast<wchar_t*>(data.data());
-  try
-  {
-    doc.parse<parse_non_destructive>(text);    // 0 means default parse flags
-  }
-  catch(std::exception const&)
-  {
-  }
+    std::wstring out;
+    int listLevel = 0;
 
-  std::wstring out;
-  html_to_text(doc.first_node(), out);
-  return out;
+    auto emit_tag = [&](std::wstring_view tag) {
+        if (tag == L"p") {
+            out += L"\n";
+        } else if (tag == L"br") {
+            out += L"\n";
+        } else if (tag == L"li") {
+            out += std::wstring(listLevel * 2, L' ') + L"- ";
+        } else if (tag == L"ul" || tag == L"ol") {
+            listLevel++;
+        } else if (tag == L"h1" || tag == L"h2" || tag == L"h3") {
+            out += L"\n";
+        }
+    };
+
+    auto emit_close_tag = [&](std::wstring_view tag) {
+        if (tag == L"p" || tag == L"h1" || tag == L"h2" || tag == L"h3") {
+            out += L"\n";
+        } else if (tag == L"ul" || tag == L"ol") {
+            listLevel = std::max(0, listLevel - 1);
+        }
+    };
+
+    size_t i = 0;
+    while (i < html.size()) {
+
+        // ---------- ESCAPES ----------
+        if (html[i] == L'&') {
+            size_t semi = html.find(L';', i + 1);
+            if (semi != std::wstring_view::npos) {
+                std::wstring_view esc = html.substr(i + 1, semi - (i + 1));
+                out += decode_escape(esc);
+                i = semi + 1;
+                continue;
+            }
+        }
+
+        // ---------- TAGS ----------
+        if (html[i] == L'<') {
+            size_t j = html.find(L'>', i + 1);
+            if (j == std::wstring_view::npos) break;
+
+            std::wstring_view tag = html.substr(i + 1, j - (i + 1));
+
+            bool closing = false;
+            if (!tag.empty() && tag[0] == L'/') {
+                closing = true;
+                tag.remove_prefix(1);
+            }
+
+            // strip attributes
+            size_t sp = tag.find(L' ');
+            if (sp != std::wstring_view::npos)
+                tag = tag.substr(0, sp);
+
+            // ignore script/style blocks
+            if (tag == L"script" || tag == L"style") {
+                std::wstring closeTag = L"</" + std::wstring(tag) + L">";
+                size_t end = html.find(closeTag, j + 1);
+                if (end == std::wstring_view::npos) return out;
+                i = end + closeTag.size();
+                continue;
+            }
+
+            if (!closing)
+                emit_tag(tag);
+            else
+                emit_close_tag(tag);
+
+            i = j + 1;
+            continue;
+        }
+
+        // ---------- TEXT ----------
+        out += html[i];
+        ++i;
+    }
+
+    return out;
 }
